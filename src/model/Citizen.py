44f73f78ade47f3b857828b4c2aa8194e58a9d50
nanashi0109 <egorgalzkov71@gmail.com>
2025-03-08 13:06:16 +0300

    feat(citizen): add fields for citizen model

--- src/model/Citizen.py
+++ src/model/Citizen.py
@@ -1,7 +1,10 @@
 from pydantic import BaseModel
 
 
-class Cityzen(BaseModel):
+class Citizen(BaseModel):
     name: str
     age: str
+    gender: str
+    education: str | None
     social_rating: str
+
