a5cb1a428d96cd28c4fa876ccecd102bc0168ee0
Force-in-Motion <canzdao@gmail.com>
2025-03-09 22:03:03 +0300

    fetch(model) added a method for changing the number of resources stored in the warehouse

--- src/data/warehouse.py
+++ src/data/warehouse.py
@@ -30,4 +30,24 @@ class ProcessingMining:
             cls.__warehouse.append(data)
 
 
+    @classmethod
+    def edit_weight_resource(cls, name: str, category : str, color: str, grade: str,  weight: float) -> bool:
+        """
+        Изменяет количество ресурса на складе если идентичные есть на складе, например если часть ресурса уже израсходована
+        :param name: принимает название ресурса,
+        :param category: принимает категорию ресурса (дерево, железо, уголь ....)
+        :param color: принимает цвет ресурса,
+        :param grade: принимает марку ресурса (A/B/C)
+        :param weight: принимает новое количество указанного ресурса на складе
+        :return: bool
+        """
+        if len(cls.__warehouse) == 0:
+            return False
+
+        for elem in cls.__warehouse:
+            if elem.name != name or elem.category != category or color != color or elem.grade != grade:
+                return False
+
+            elem.weight += weight
+            return True
 
