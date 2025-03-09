736e90ca045ec0c3329c125739198004b5a2794a
Force-in-Motion <canzdao@gmail.com>
2025-03-09 22:02:06 +0300

    fetch(model) Added a method for adding data to the department of statistics and storage of resources

--- src/data/warehouse.py
+++ src/data/warehouse.py
@@ -6,5 +6,28 @@ class ProcessingMining:
 
     __statistic = []
 
+    @classmethod
+    def add_resource(cls, data: Resource) -> None:
+        """
+        Добавляет Полученные ресурсы на склад на склад, суммирует количество добытых ресурсов если идентичные есть на складе,
+        Также добавляет данные о ежедневной добыче ресурсов в отдел статистики
+        :param data: принимает сведения о добытых ресурсах за указанную дату
+        :return: None
+        """
+        cls.__statistic.append(data)
+
+        del data['date']
+
+        if len(cls.__warehouse) != 0:
+
+            for elem in cls.__warehouse:
+                if elem.name != data.name or elem.category != data.category or elem.color != data.color or elem.grade != data.grade:
+                    cls.__warehouse.append(data)
+
+                else:
+                    elem.weight += data.weight
+        else:
+            cls.__warehouse.append(data)
+
 
 
