dc10871dafa94d903fedcbde15fae898a56d6fa3
Force-in-Motion <canzdao@gmail.com>
2025-03-09 20:13:35 +0300

    fix(model): a separate entity is removed by coal, now the essence of the ore is responsible for the name of the fossils and its description

--- src/model/iron.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class Iron(BaseModel):
-    name: str # Название руды
-    date: str # Дата выработки
-    weight: float  # Вес в килограммах, выработка за день
-    color: str  # Цвет руды
-    brand: str  # Марка руды
