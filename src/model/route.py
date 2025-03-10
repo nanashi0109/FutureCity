ae0dd54561ee68880e5cbc361c36cc0db3baeeb5
suchok94 <suchok94@gmail.com>
2025-03-10 13:55:30 +0300

    delete(model) delete files: route, type_transport

--- src/model/route.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class Route(BaseModel):
-    id: int
-    name: str | None = None
-    count_stops: int
-    
--- src/model/type_transport.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class Type_Transoprt(BaseModel):
-    name: str
-    
