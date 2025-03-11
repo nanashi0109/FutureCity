9a43def6df819eb6f435406f3c9e8e22ba8fc96b
krishtalevia <krishtalev.ilya@gmail.com>
2025-03-11 20:44:48 +0300

    feat(teacher): add data methods for teacher

--- src/data/school_system/teacher.py
+++ src/data/school_system/teacher.py
@@ -4,18 +4,40 @@ class Teachers:
     __teachers = []
     
     @classmethod
-    def get_one(cls, name: str) -> Teacher | None:
+    def get_one(cls, id: int) -> Teacher | None:
         for teacher in cls.__teachers:
-            pass
+            if teacher.id == id:
+                return teacher
 
     @classmethod
     def get_all(cls) -> list[Teacher]:
-        pass
+        return cls.__teachers
 
     @classmethod
-    def create(cls, creature: Teacherr) -> bool:
-        pass
+    def create(cls, teacher_to_add: Teacher) -> bool:
+        insert_id = 1
+        for teacher in cls.__teachers:
+            if teacher.id == insert_id:
+                insert_id += 1
+            else:
+                break
+
+        teacher_to_add.id = insert_id
+
+        cls.__teachers.append(teacher_to_add)
+        return True
 
     @classmethod
     def delete(cls, id: int) -> bool:
-        pass
+        for teacher in cls.__teachers:
+            if teacher.id == id:
+                cls.__teachers.remove(teacher)
+                return True
+        return False
+
+    @classmethod
+    def update(cls, teacher_to_update: Teacher) -> bool:
+        for teacher in cls.__teachers:
+            if teacher.id == teacher_to_update.id:
+                teacher.name = teacher_to_update.name
+                teacher.school_id = teacher_to_update.school_id
