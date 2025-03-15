526cf89cb0fb7dc7402ce192e2b2d01a51b25891
suchok94 <suchok94@gmail.com>
2025-03-15 12:52:00 +0300

    feat(test) add test update routes in test_routes.py

--- src/test/data/transport_network/test_routes.py
+++ src/test/data/transport_network/test_routes.py
@@ -51,7 +51,6 @@ def test_delete(id, extend_result):
     data = Routes.get_all_routes()
     assert  data == extend_result
 
-# create_route
 @pytest.mark.parametrize('data, extend_result', [
     (Route(id= 6, name='Route 6', count_stops=5, count_transport_on_line=0),
      [
@@ -70,7 +69,20 @@ def test_create(data, extend_result):
     assert  data == extend_result
 
 
+@pytest.mark.parametrize('update_route, extend_result', [
+    (Route(id= 6, name='Route 1', count_stops=5, count_transport_on_line=0),
+     [
+        Route(id= 2, name='Route 1', count_stops=5, count_transport_on_line=0),
+        Route(id= 3, name='Route 1', count_stops=5, count_transport_on_line=0),
+        Route(id= 4, name='Route 1', count_stops=5, count_transport_on_line=0),
+        Route(id= 5, name='Route 1', count_stops=5, count_transport_on_line=0),
+        Route(id= 6, name='Route 1', count_stops=5, count_transport_on_line=0)
+        ])
+    ])
 
-
-
+def test_update(update_route, extend_result):
+    
+    
+    Routes.update(update_route)
+    assert  Routes.get_all_routes() == extend_result
 
