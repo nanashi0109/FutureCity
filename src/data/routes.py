c49eb2c7119e08e57cbe328d3702119a299c2b0a
suchok94 <suchok94@gmail.com>
2025-03-15 12:08:18 +0300

    fix(data) remove function update route

--- src/data/routes.py
+++ src/data/routes.py
@@ -1,21 +1,21 @@
-from model.transport_network.route import Route
+from src.model.transport_network.route import Route
 
 class Routes:
     __routes = []
 
     @classmethod
-    async def get_one_route(cls, id: int) -> Route | None:
+    def get_one_route(cls, id: int) -> Route | None:
         for route in cls.__routes:
             if route.id == id:
                 return route
         return
     
     @classmethod
-    async def get_all_routes(cls) -> list[Route]:
+    def get_all_routes(cls) -> list[Route]:
         return cls.__routes
 
     @classmethod
-    async def create_route(cls, route: Route) -> None:
+    def create_route(cls, route: Route) -> None:
         for route_i in cls.__routes:
             if route_i.id == route.id:
                 route.id = len(cls.__transports) + 1
@@ -23,14 +23,14 @@ class Routes:
         cls.__routes.append(route)
 
     @classmethod
-    async def delete_route(cls, id: int) -> None:
+    def delete_route(cls, id: int) -> None:
         for route in cls.__routes:
             if route.id == id:
                 cls.__routes.remove(route)
         
     
-    @classmethod
-    async def update(cls, route: Route) -> None:
-        for route_i in cls.__routes:
-            if route_i.id == route.id:
-                cls.__routes(route_i) = route
+    # @classmethod
+    # async def update(cls, route: Route) -> None:
+    #     for route_i in cls.__routes:
+    #         if route_i.id == route.id:
+    #             cls.__routes(route_i) = route
