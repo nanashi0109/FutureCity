6c83f7f38b8e5d266cf5b2e8ca6ec213559948d2
suchok94 <suchok94@gmail.com>
2025-03-15 10:56:01 +0300

    refactor(data) changes functions in routes.py to async

--- src/data/routes.py
+++ src/data/routes.py
@@ -4,18 +4,18 @@ class Routes:
     __routes = []
 
     @classmethod
-    def get_one_route(cls, id: int) -> Route | None:
+    async def get_one_route(cls, id: int) -> Route | None:
         for route in cls.__routes:
             if route.id == id:
                 return route
         return
     
     @classmethod
-    def get_all_routes(cls) -> list[Route]:
+    async def get_all_routes(cls) -> list[Route]:
         return cls.__routes
 
     @classmethod
-    def create_route(cls, route: Route) -> None:
+    async def create_route(cls, route: Route) -> None:
         for route_i in cls.__routes:
             if route_i.id == route.id:
                 route.id = len(cls.__transports) + 1
@@ -23,14 +23,14 @@ class Routes:
         cls.__routes.append(route)
 
     @classmethod
-    def delete_route(cls, id: int) -> None:
+    async def delete_route(cls, id: int) -> None:
         for route in cls.__routes:
             if route.id == id:
                 cls.__routes.remove(route)
         
     
     @classmethod
-    def update(cls, route: Route) -> None:
+    async def update(cls, route: Route) -> None:
         for route_i in cls.__routes:
             if route_i.id == route.id:
                 cls.__routes(route_i) = route
