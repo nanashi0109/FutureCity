3ffc662c676cc1b58f5be1e1e6c670d9b98d4ae8
suchok94 <suchok94@gmail.com>
2025-03-15 10:56:19 +0300

    refactor(data) changes functions in transports.py to async

--- src/data/transports.py
+++ src/data/transports.py
@@ -4,18 +4,18 @@ class Transports:
     __transports = []
 
     @classmethod
-    def get_one_transport(cls, id: int) -> Transport | None:
+    async def get_one_transport(cls, id: int) -> Transport | None:
         for transport in cls.__transports:
             if transport.id == id:
                 return transport
         return
     
     @classmethod
-    def get_all_transports(cls) -> list[Transport]:
+    async def get_all_transports(cls) -> list[Transport]:
         return cls.__transports
 
     @classmethod
-    def create_transport(cls, transport: Transport) -> None:
+    async def create_transport(cls, transport: Transport) -> None:
         for transport_i in cls.__transports:
             if transport_i.id == transport.id:
                 transport.id = len(cls.__transports) + 1
@@ -23,14 +23,14 @@ class Transports:
         cls.__transports.append(transport)
 
     @classmethod
-    def delete_transport(cls, id: int) -> None:
+    async def delete_transport(cls, id: int) -> None:
         for transport in cls.__transports:
             if transport.id == id:
                 cls.__transports.remove(transport)
         
     
     @classmethod
-    def update(cls, transport: Transport) -> None:
+    async def update(cls, transport: Transport) -> None:
         for transport_i in cls.__transports:
             if transport_i.id == transport.id:
                 cls.__transports(transport_i) = transport
