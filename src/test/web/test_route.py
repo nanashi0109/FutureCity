d36be29a564dea3a93a7da36105698c9c9d92eee
suchok94 <suchok94@gmail.com>
2025-03-17 20:31:19 +0300

    feat(test) add test for web(not working)

--- src/test/web/test_route.py
+++ src/test/web/test_route.py
@@ -1,29 +1,8 @@
-# import pytest
-# import asyncio
-
-# from fastapi import APIRouter, Body
-
-# @pytest.fixture(scope='module', autouse=True)
-# def setup():
-#     server = asyncio.create_subprocess_exec('uvicorn', 'main.app', '--host', '127.0.0.1', '--port', '8000')
-
-#     asyncio.sleep(2)
-
-#     yield None
-
-#     server.close()
-
-
-# def test_get_all(setup):
-#     result = get('/')
-#     assert result == []
-
-
-
 import pytest
 import time
 import subprocess
 import requests
+from src.model.transport_network.route import Route
 
 # Эта фикстура будет вызвана один раз перед началом выполнения тестов в данном модуле
 # И второй раз после выполнения всех тестов (чтоб закрыть сервер)
@@ -58,3 +37,18 @@ def test_some():
     response = requests.get("http://127.0.0.1:8000/route")
     assert response.status_code == 200
     assert response.json() == {"status": "ok", "routes": []}
+
+def test_add_route():
+    response = requests.post("http://127.0.0.1:8000/route/add-route", data= Route(id= 1, name='Route 1', count_stops=5, count_transport_on_line=0))
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
+
+def test_update_route():
+    response = requests.patch("http://127.0.0.1:8000/route/update-route/{1}", data= Route(id= 1, name='Route 11', count_stops=51, count_transport_on_line=2))
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
+
+def test_delete_route():
+    response = requests.delete("http://127.0.0.1:8000/route/delete-route/{1}")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
